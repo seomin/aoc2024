f2edc97cbf4d908fc824e28987262848bfe568b5
Simeon Kaul <simeon.kaul@devboost.com>
2024-12-07 21:16:36 +0100

    d4p2

--- 04.lua
+++ 04.lua
@@ -29,7 +29,30 @@ local function mas(r, c, dr, dc)
 	return 0
 end
 
+local function crossMas(r, c)
+	if
+		not (
+			(charAt(r - 1, c - 1, "M") and charAt(r + 1, c + 1, "S"))
+			or (charAt(r - 1, c - 1, "S") and charAt(r + 1, c + 1, "M"))
+		)
+	then
+		-- print("no \\ at", r, c)
+		return 0
+	end
+	if
+		not (
+			(charAt(r - 1, c + 1, "M") and charAt(r + 1, c - 1, "S"))
+			or (charAt(r - 1, c + 1, "S") and charAt(r + 1, c - 1, "M"))
+		)
+	then
+		-- print("no / at", r, c)
+		return 0
+	end
+	return 1
+end
+
 local xmas = 0
+local x_mas = 0
 for r, row in pairs(m) do
 	for c, char in pairs(row) do
 		if char == "X" then
@@ -41,8 +64,11 @@ for r, row in pairs(m) do
 			xmas = xmas + mas(r, c, 1, -1)
 			xmas = xmas + mas(r, c, 1, 0)
 			xmas = xmas + mas(r, c, 1, 1)
+		elseif char == "A" then
+			x_mas = x_mas + crossMas(r, c)
 		end
 	end
 end
 
 print("p1", xmas)
+print("p2", x_mas)

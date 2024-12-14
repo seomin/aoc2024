ac4e666b1fec854285e43f4dccba44b6c36b946a
Simeon Kaul <simeon.kaul@devboost.com>
2024-12-14 22:39:51 +0100

    d14p2

--- 14.lua
+++ 14.lua
@@ -24,9 +24,11 @@ local function nums(str)
 	return table.unpack(result)
 end
 
+local robots = {}
 local q = { 0, 0, 0, 0 }
 for line in io.lines(file) do
 	local px, py, vx, vy = nums(line)
+	table.insert(robots, {px, py, vx, vy})
 	local finalx = math.fmod(math.fmod(px + T * vx, X) + X, X)
 	local finaly = math.fmod(math.fmod(py + T * vy, Y) + Y, Y)
 	-- print(finalx, finaly)
@@ -50,4 +52,56 @@ end
 p1 = q[1] * q[2] * q[3] * q[4]
 
 print("p1", p1)
-print("p2", p2)
+
+local function ins(board, x, y)
+	if not board[x] then
+		board[x] = { y = 1}
+	elseif not board[x][y] then
+		board[x][y] = 1
+	else
+		board[x][y] = board[x][y] + 1
+	end
+end
+local function show()
+	local board = {}
+	for _, robot in pairs(robots) do
+		ins(board, robot[1], robot[2])
+	end
+	for y = 0, Y do
+		local line = ""
+		for x = 0, X do
+			local char = "."
+			if board[x] and board[x][y] then
+				char = "x"
+			end
+			line = line .. char
+		end
+		print(line)
+	end
+end
+
+local function update()
+	for _, robot in pairs(robots) do
+		robot[1] = math.fmod(robot[1] + robot[3] + X, X)
+		robot[2] = math.fmod(robot[2] + robot[4] + Y, Y)
+	end
+end
+
+-- These numbers showed interesting patterns
+-- 23 + 103*t
+-- 48 + 101*t
+
+local t = 0
+show()
+while true do
+	t = t + 1
+	update()
+	while math.fmod(t-23, 103) ~= 0 and math.fmod(t-48, 101) ~= 0 do
+		t = t + 1
+		update()
+	end
+	print("t", t)
+	show()
+	_ = io.read()
+end
+

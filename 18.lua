ac88cfa0cfc457d52e102db279e1033ea854bb4c
Simeon Kaul <simeon.kaul@devboost.com>
2024-12-20 11:58:40 +0100

    d18p2

--- 18.lua
+++ 18.lua
@@ -1,6 +1,5 @@
 #!/usr/bin/env lua
 
-local p2 = 0
 local file = "18.txt"
 D = 70
 C = 1024
@@ -29,10 +28,13 @@ for y = 0, D do
 end
 
 local c = 0
-for line in io.lines(file) do
+local f = io.open(file)
+assert(f)
+while true do
 	if c == C then
 		break
 	end
+	local line = f:read()
 	local x, y = nums(line)
 	G[y][x] = "#"
 	c = c + 1
@@ -45,36 +47,50 @@ local function key2(x, y)
 	return x .. ":" .. y
 end
 
-Q = { key(0, 0, 0) }
-SEEN = {}
-DIRS = {{1,0}, {0,1}, {-1, 0}, {0, -1}}
+local function solve()
+	Q = { key(0, 0, 0) }
+	SEEN = {}
+	DIRS = {{1,0}, {0,1}, {-1, 0}, {0, -1}}
 
-local k = table.remove(Q, 1)
-while k do
-	Q[k] = nil
-	local x, y, d = nums(k)
-	if SEEN[key2(x, y)] then
-		k = table.remove(Q, 1)
-		goto continue
-	end
-	if x == D and y == D then
-		print("p1", d)
-		break
-	end
-	SEEN[key2(x, y)] = true
+	local k = table.remove(Q, 1)
+	while k do
+		Q[k] = nil
+		local x, y, d = nums(k)
+		if SEEN[key2(x, y)] then
+			k = table.remove(Q, 1)
+			goto continue
+		end
+		if x == D and y == D then
+			return d
+		end
+		SEEN[key2(x, y)] = true
 
-	for _, dir in pairs(DIRS) do
-		local dx, dy = table.unpack(dir)
-		local nx, ny = x+dx, y+dy
-		if G[ny] and G[ny][nx] == "." then
-			if not SEEN[key2(nx, ny)] then
-				Q[#Q+1] = key(nx, ny, d+1)
+		for _, dir in pairs(DIRS) do
+			local dx, dy = table.unpack(dir)
+			local nx, ny = x+dx, y+dy
+			if G[ny] and G[ny][nx] == "." then
+				if not SEEN[key2(nx, ny)] then
+					Q[#Q+1] = key(nx, ny, d+1)
+				end
 			end
 		end
-	end
 
-	k = table.remove(Q, 1)
-    ::continue::
+		k = table.remove(Q, 1)
+	    ::continue::
+	end
+	return nil
 end
 
-print("p2", p2)
+print("p1", solve())
+
+
+while true do
+	local line = f:read()
+	assert(line, "unreachable")
+	local x, y = nums(line)
+	G[y][x] = "#"
+	if not solve() then
+		print("p2", x..","..y)
+		break
+	end
+end
